9cd6161959738ef916e67f3e0d2d9a8c66863135
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2020-05-06 00:40:30 -0700

    Add files via upload
    Day 2: I worked on the code once again, and I addressed a lot of bugs that I didn't notice on the first day. I also added a lot of comments so it's easier to understand the purpose and goal of each function. I still have a looping error that I can't seem to figure out what is causing the problem. Initially I thought it was because of the variable, but after some debugging, it doesn't seem to be the case. I may have to look towads the internet or office hours for help.

--- lab08.py
+++ lab08.py
@@ -4,8 +4,11 @@ prime_list=[]
 d=2
 number = 0
 def is_prime(n):
+    """ is_prime is a function that determines whether or not a number is prime 
+    It works by determining if the number is divisible by anything other than 1 or itself. 
+    It does this by making sure everything has a remainder of more than 0 and makes sure that
+    once the divisor is equal to the number being divided, it ends the function and returns True"""
     global d
-    print(d)
     if n%d == 0:
         return False
     elif n == d:
@@ -15,13 +18,18 @@ def is_prime(n):
         is_prime(n)
     
 def find_prime():
+    """ findprime is a function dedicated to finding the 10001st prime number
+    It does this by determining whether all number leading up to the 10001st prime number
+    are prime or not, putting it into a list to be able to determine when to return 
+    the number instead of continuing. """
     global number
-    print(number)
     if len(prime_list)==10001:
         return prime_list[10000]
     else:
         if is_prime(number) == True:
             prime_list.append(number)
+            number += 1
+            prime_list()
         else:
             number +=1
             find_prime() 
