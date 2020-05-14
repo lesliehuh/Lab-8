d150d49fee0ea279c37f78e69d35a9cff05f0723
lesliehuh <57726465+lesliehuh@users.noreply.github.com>
2020-05-13 23:03:09 -0700

    Add files via upload
    I was not able to change a lot of the actual program this time around. It took me a while to figure out the program was continuously returning true for numbers that weren't prime numbers. However, I was able to figure out what was wrong after deleting a bit of code and using Python Tutor to visualize the program. In the end, the programming is working as anticipated except it is returning system overflow. I need to figure out a way to get around this or use an alternative method that I can optimize the program. Worse comes to worse, I think I will end up changing the is_prime program to the module that has the program built in.

--- lab08.py
+++ lab08.py
@@ -1,29 +1,46 @@
 #Leslie Huh
 #Lab 08
-prime_list=[]
-d=2
-number = 0
+import sys
+sys.setrecursionlimit(15000)
+prime_list=[1]
+d=1
+number = 2
 def is_prime(n):
+    """ is_prime is a function that determines whether or not a number is prime 
+    It works by determining if the number is divisible by anything other than 1 or itself. 
+    It does this by making sure everything has a remainder of more than 0 and makes sure that
+    once the divisor is equal to the number being divided, it ends the function and returns True"""
+    global prime_list
     global d
-    print(d)
-    if n%d == 0:
-        return False
-    elif n == d:
+    if len(prime_list) == 1:
+        d=1
         return True
+    elif len(prime_list)==d+1:
+        d=1
+        return True
+    elif n%prime_list[d]==0:
+        d=1       
+        return False
     else:
         d+=1
-        is_prime(n)
+        return is_prime(n)
     
 def find_prime():
+    """ findprime is a function dedicated to finding the 10001st prime number
+    It does this by determining whether all number leading up to the 10001st prime number
+    are prime or not, putting it into a list to be able to determine when to return 
+    the number instead of continuing. """
     global number
-    print(number)
     if len(prime_list)==10001:
         return prime_list[10000]
     else:
         if is_prime(number) == True:
             prime_list.append(number)
+            number += 1
+            find_prime()
         else:
             number +=1
             find_prime() 
+            
 
-print(find_prime())
+print (find_prime())
